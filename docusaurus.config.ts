2815c3eeb496ec08093976866ba1fad3d8e22384
S. Isaac Kellogg <isaac.kellogg@quantellia.com>
2024-09-23 10:03:53 -0400

    reconfig

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/opendi-icon-small.png',
 
   // Set the production url of your site here
-  url: 'http://api.opendi.org',
+  url: 'http://iamkeldev.github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/test-opendi-api/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'opendi-org', // Usually your GitHub org/user name.
-  projectName: 'api', // Usually your repo name.
+  organizationName: 'IAmKelDev', // Usually your GitHub org/user name.
+  projectName: 'test-opendi-api', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
